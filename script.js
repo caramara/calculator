46c98761b0e554edde5c03153be5472ab79d3548
caramara <174440721+caramara@users.noreply.github.com>
2024-08-04 17:03:17 +0300

    Add calculator operations in js

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 //Initialize variables
 const display = document.querySelector('#display');
 const buttonsContainer = document.querySelector('#buttons-container');
-let operator = '';
-let op1 = '';
-let op2 = '';
 
 createCalc();
 buttonsContainer.addEventListener('click', (e) => {
     const target = e.target;
-    display.innerText += target.innerText;
+
+    if(target.innerText==='='){
+        display.innerText = operate(display.innerText);
+    }
+    else if(target.innerText==='Clear'){
+        display.innerText = '';
+    }
+    else{
+        display.innerText += target.innerText;
+    }
 })
 
 function createCalc(){
@@ -39,19 +45,30 @@ function createCalc(){
 
 }
 
-function operate(operator, op1, op2){
-    switch(operator){
+function operate(string){
+
+    const operands = string.split(/[\+\-\*\/]/).map((e)=>parseFloat(e));
+    const operators = string.match(/[\+\-\*\/]/g);
+    let operator = operators[0];
+
+    while(operands.length>2){
+        let calculation = operands[0] + operators[0] + operands[1];
+        operands.splice(0,2,operate(calculation));
+        operators.shift();
+        //operands.unshift(operate(calculation));
+    }
+    switch(operators[0]){
         case '+':
-            return add(op1, op2);
+            return add(operands[0], operands[1]);
             
         case '-':
-            return subtract(op1, op2);
+            return subtract(operands[0], operands[1]);
             
         case '*':
-            return multiply(op1, op2);
+            return multiply(operands[0], operands[1]);
             
         case '/':
-            return divide(op1, op2);
+            return divide(operands[0], operands[1]);
             
     }
 
@@ -60,12 +77,15 @@ function operate(operator, op1, op2){
 function add(a, b){
     return a+b;
 }
-function subtract(a, b ){
+function subtract(a, b){
     return a-b;
 }
 function multiply(a, b){
     return a*b;
 }
 function divide(a, b){
+    if(b===0){
+        throw new Error('Division by zero');
+    }
     return a/b;
 }
